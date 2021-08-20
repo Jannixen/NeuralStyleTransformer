2cb9d598467b9d9355e63bbf13c99bc12dcb7230
Jannixen <jannixen@gmail.com>
2021-08-20 21:49:53 +0200

    Integrating NeuralStyleTransformerModel with window

--- model.py
+++ model.py
@@ -9,9 +9,12 @@ class NeuralStyleTransformerModel:
     def __init__(self, user_image_path, style_image_path, style_image_size=(256, 256), output_image_size=(384, 384)):
         self.user_image = self.load_image(user_image_path, output_image_size)
         self.style_image = self.load_image(style_image_path, style_image_size)
-        self.make_output_image()
+        self.stylized_image = self.make_stylized_image()
 
-    def make_output_image(self):
+    def get_stylized_image(self):
+        return self.stylized_image
+
+    def make_stylized_image(self):
         style_image = tf.nn.avg_pool(self.style_image, ksize=[3, 3], strides=[1, 1], padding='SAME')
 
         hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
@@ -20,8 +23,7 @@ class NeuralStyleTransformerModel:
         outputs = hub_module(tf.constant(self.user_image), tf.constant(style_image))
         stylized_image = outputs[0]
 
-        self.show_n([self.user_image, style_image, stylized_image],
-                    titles=['Original content image', 'Style image', 'Stylized image'])
+        return tf.keras.preprocessing.image.array_to_img(stylized_image[0])
 
     @staticmethod
     def crop_center(image):
@@ -33,11 +35,10 @@ class NeuralStyleTransformerModel:
             image, offset_y, offset_x, new_shape, new_shape)
         return image
 
-    def load_image(self, image_url, image_size=(256, 256), preserve_aspect_ratio=True):
-        with open(image_url, encoding="utf8", errors='ignore') as f:
-            img = tf.io.decode_image(
-                tf.io.read_file(image_url),
-                channels=3, dtype=tf.float32)[tf.newaxis, ...]
+    def load_image(self, image_url, image_size=(256, 256)):
+        img = tf.io.decode_image(
+            tf.io.read_file(image_url),
+            channels=3, dtype=tf.float32)[tf.newaxis, ...]
         img = self.crop_center(img)
         img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
         return img
--- transformer_window.py
+++ transformer_window.py
@@ -1,17 +1,23 @@
+import os
 from functools import partial
+from io import BytesIO
 
 from kivy.app import App
+from kivy.core.image import Image as CoreImage
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
+from kivy.uix.filechooser import FileChooserIconView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+
+from model import NeuralStyleTransformerModel
 
 STYLES_TUPLE = ("Beksinski", "Kandinsky", "Mucha", "Picasso", "The Great Wave off Kanagawa", "Van Gogh")
 INITIAL_USER_IMAGE = "styles/init_user_image.jpg"
 INITIAL_STYLE_IMAGE = "styles/Picasso.jpg"
-INITIAL_RESULT_IMAGE = "styles/black.jpg"
 
 
 class NeuralStyleTransformerApp(App):
@@ -19,19 +25,41 @@ class NeuralStyleTransformerApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.bottom_layout = BoxLayout(size_hint=(1, None), height=50)
-        self.display_layout = GridLayout(cols=2, padding=50)
+        self.display_layout = GridLayout(cols=3, padding=50)
         self.title_layout = BoxLayout(size_hint=(1, None), height=50)
         self.style_image = Image(source=INITIAL_STYLE_IMAGE, width=300, height=300)
         self.user_image = Image(source=INITIAL_USER_IMAGE, width=300, height=300)
-        self.result_image = Image(source=INITIAL_RESULT_IMAGE, width=300, height=300)
 
     def display_style_image(self, style, *args):
         image_dir = 'styles/' + style.replace(" ", "_") + '.jpg'
         self.style_image = Image(source=image_dir, width=300, height=300)
         self.rebuild_display_layout()
 
-    def choose_image(self):
-        pass
+    def choose_image(self, *args):
+        image_chooser = FileChooserIconView(filters=["*.png", "*.jpg"], path=os.getcwd())
+        image_chooser.bind(on_submit=partial(self.update_user_image, image_chooser))
+        popup = Popup(title='Choose file',
+                      content=image_chooser,
+                      size_hint=(None, None), size=(500, 500))
+        popup.open()
+
+    def update_user_image(self, image_chooser, *args):
+        image_path = image_chooser.selection[0]
+        self.user_image = Image(source=image_path, width=300, height=300)
+        self.rebuild_display_layout()
+
+    def transform(self, *args):
+        transformer = NeuralStyleTransformerModel(self.user_image.source, self.style_image.source)
+        pil_image = transformer.get_stylized_image()
+        img_bytes = BytesIO()
+        pil_image.save(img_bytes, format='PNG')
+        img_bytes.seek(0)
+        img = CoreImage(BytesIO(img_bytes.read()), ext='png')
+
+        popup = Popup(title='Stylized image',
+                      content=Image(texture=img.texture),
+                      size_hint=(None, None), size=(400, 400))
+        popup.open()
 
     def build_style_dropdown(self):
         style_dropdown = DropDown()
@@ -48,22 +76,25 @@ class NeuralStyleTransformerApp(App):
         return style_dropdown_button
 
     def build_title_layout(self):
-        title_label = Label(text='Neural Style Transformer', height=50, font_size='20sp')
+        title_label = Label(text='Neural Style Transformer', height=50, font_size='30sp')
         self.title_layout.add_widget(title_label)
 
     def build_bottom_layout(self):
         image_chooser_button = Button(text='Choose image',
-                                      on_press=partial(self.choose_image))
+                                      on_press=self.choose_image)
+
+        transformation_button = Button(text='Start transformation',
+                                       on_press=self.transform)
 
         self.bottom_layout.add_widget(image_chooser_button)
         self.bottom_layout.add_widget(self.build_style_dropdown())
+        self.bottom_layout.add_widget(transformation_button)
 
     def rebuild_display_layout(self):
         self.display_layout.clear_widgets()
         self.display_layout.add_widget(self.user_image)
+        self.display_layout.add_widget(Image(source="styles/button.png", width=50, height=50))
         self.display_layout.add_widget(self.style_image)
-        self.display_layout.add_widget(Image(source="styles/arrow.png", width=150, height=150))
-        self.display_layout.add_widget(self.result_image)
 
     def build(self):
         self.build_title_layout()
